8dbc50d0b679fd11e9b927d35d8b97fe0abaf140
Anna Savrova <annsavrova@gmail.com>
2024-05-05 21:33:10 +0700

    Refactor code, improve method save and implement multithreading in PostRepository

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
@@ -18,24 +20,47 @@ public class PostController {
 
   public void all(HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
+    List<Post> data = service.all();
+    writeResponse(response, data);
   }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
-
-  public void save(Reader body, HttpServletResponse response) throws IOException {
+  public void getById(HttpServletRequest req, HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
+    long id = extractPostIdFromURL(req.getRequestURI());
+    Post post = service.getById(id);
+    if (post != null) {
+      writeResponse(response, post);
+    } else {
+      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+  }
+
+  public void save(HttpServletRequest req, HttpServletResponse response) throws IOException {
+    response.setContentType(APPLICATION_JSON);
+    Gson gson = new Gson();
+    try (Reader reader = req.getReader()) {
+      Post post = gson.fromJson(reader, Post.class);
+      Post savedPost = service.save(post);
+      writeResponse(response, savedPost);
+    } catch (Exception e) {
+      response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+      response.getWriter().print(gson.toJson("Invalid request body"));
+    }
+  }
+
+  public void removeById(HttpServletRequest req, HttpServletResponse response) {
+    response.setContentType(APPLICATION_JSON);
+    long id = extractPostIdFromURL(req.getRequestURI());
+    service.removeById(id);
+    response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+  }
+
+  private void writeResponse(HttpServletResponse response, Object data) throws IOException {
+    Gson gson = new Gson();
     response.getWriter().print(gson.toJson(data));
   }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
+  private long extractPostIdFromURL(String url) {
+    return Long.parseLong(url.substring(url.lastIndexOf("/") + 1));
   }
 }
--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -13,38 +13,33 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
+    final PostRepository repository = new PostRepository();
+    final PostService service = new PostService(repository);
     controller = new PostController(service);
   }
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
     try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals("GET") && path.equals("/api/posts")) {
-        controller.all(resp);
+      final String path = req.getRequestURI();
+      final String method = req.getMethod();
+
+      if (method.equals("GET")) {
+        if (path.equals("/api/posts")) {
+          controller.all(resp);
+          return;
+        } else if (path.matches("/api/posts/\\d+")) {
+          controller.getById(req, resp);
+          return;
+        }
+      } else if (method.equals("POST") && path.equals("/api/posts")) {
+        controller.save(req, resp);
+        return;
+      } else if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+        controller.removeById(req, resp);
         return;
       }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
-        controller.save(req.getReader(), resp);
-        return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
+
       resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
     } catch (Exception e) {
       e.printStackTrace();
